d1bae41aac3003fc41f4c1fb7d74e2926da417a4
ardhrubo <ardhrubo3038@gmail.com>
2025-05-03 10:34:04 +0600

    Refactor user account types and implement AppwriteService methods

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -1,5 +1,5 @@
 import conf from '@/conf/config';
-import {Client,Account,ID,Databases} from 'appwrite'
+import {Client,Account,ID} from 'appwrite'
 
 type CreateUserAccound = {
     email: string,
@@ -8,7 +8,7 @@ type CreateUserAccound = {
 }
 
 
-type LoginUserAccound = {
+type LoginUserAccount = {
     email: string,
     password: string,
 }
@@ -25,4 +25,49 @@ appwrteClient
     .setProject(conf.appwriteProjectId)
 
 export const account = new Account(appwrteClient);
-export const databases = new Databases(appwrteClient);
+
+export class AppwriteService {
+    // new record of user inside appwrite
+
+    async createUserAccount({email,password,name}:CreateUserAccound){
+        try {
+           const userAccount =  await account.create(ID.unique(),email,password,name)
+           if(userAccount){
+            
+           }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    // login
+
+    async login({email,password}:LoginUserAccount){
+        try {
+            
+        } catch (error) {
+            throw error
+            
+        }
+    }
+
+    
+    async isLoggedIn(){
+        
+    }
+    
+    async updateUser({email,password}:UpdatedUserAccount){
+
+    }
+
+    async logout(){
+
+    }
+
+}
+
+
+
+
+
+
