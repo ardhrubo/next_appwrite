806dc1ff4c7c493881fa7a9ad163d09b3863e7d4
ardhrubo <ardhrubo3038@gmail.com>
2025-05-06 20:20:32 +0600

    feat: add authentication pages and components, including ProtectedLayout, ProfileCard, and updated Login and Signup forms

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -116,7 +116,7 @@ const Signup = () => {
                         <div>
                             <button
                                 type="submit"
-                                className="inline-flex w-full items-center justify-center rounded-md bg-primary px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-primary/80"
+                                className="inline-flex w-full items-center justify-center rounded-md bg-[#f02e65] px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-[#f02e65]/80"
                             >
                                 Create Account
                             </button>
